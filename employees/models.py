afbb7cce6beef54b28c73cd980a18c2651d19deb
siboniseni <siboniseni.kasa@outlook.com>
2025-01-12 19:34:09 +0200

    Employee model created

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    position = models.CharField(max_length=50)
+    working_hours = models.PositiveIntegerField()
+    days_off = models.TextField()  # To store a JSON of days
+
+    def __str__(self):
+        return self.name
